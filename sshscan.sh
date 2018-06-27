0525dda16abce81237fdb1e52686e0a85d7f4d9a
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-06-27 15:11:45 +0200

    Update sshscan.sh

--- sshscan.sh
+++ sshscan.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
+# 
+# Extrae fingerprint version SSH, en fichero ip.txt
+# hackingyseguridad.com 
+# sleep 1 telnet IP puerto 
+#
+#!/bin/sh
+
 for n in `cat ip.txt`; do echo $n; timeout --signal=9 2 telnet $n 22 |grep SSH; done
