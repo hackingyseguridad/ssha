f1f0e30d364329d2e6b04b863e2757509c79f184
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-08-27 22:29:18 +0200

    Update sshscan.sh

--- sshscan.sh
+++ sshscan.sh
@@ -1,4 +1,19 @@
 # 
+#
+#EXTRAER VERSION DE SSH EN LOCAL Y REMOTO
+#
+#En local:
+#
+#ssh -V 
+#sshd -V
+#
+#En remoto:
+#nmap -Pn -p 22 -sV --script=ssh-run IP
+#
+#sleep 1 telnet IP 22
+#
+#python ssh-audit.py -1 IP #https://github.com/arthepsy/ssh-audit
+#
 # Extrae fingerprint version SSH, en fichero ip.txt
 # hackingyseguridad.com 
 # sleep 1 telnet IP puerto 
