1499c7396ac2a1b4eba699492c937742ebaa38b1
hackingyseguridad <antonio_taboada@hotmail.es>
2019-12-18 21:23:38 +0100

    Update nmapxsalto.sh

--- nmapxsalto.sh
+++ nmapxsalto.sh
@@ -1,8 +1,8 @@
-
-
-
+#!/bin/bash
+#nmap a traves de una maquina de salto
+#MiPC----PCSALTO----OBJETIVO
 # configuramos en local en etc/proxychains.conf socks4  127.0.0.1 9050
 
 ssh -f admin@SALTO -L 9050:localhost:222 -N 
-
-sudo nmap 192.168.1.1 -Pn -v --proxy socks4://127.0.0.1:9050 -sTV -F -reason
+sudo nmap OBJETIVO -Pn -v --proxy socks4://127.0.0.1:9050 -sTV -F -reason
+sudo proxychains -q nmap -Pn OBJETIVO -sTV -reason
