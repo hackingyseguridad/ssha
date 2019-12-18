046792e3190483126c3f82dbe97d20f22628bac9
hackingyseguridad <antonio_taboada@hotmail.es>
2019-12-18 21:25:02 +0100

    Update nmapxsalto.sh

--- nmapxsalto.sh
+++ nmapxsalto.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#nmap a traves de una maquina de salto
-#MiPC----PCSALTO----OBJETIVO
+# nmap a traves de una maquina de salto
+# MiPC ----- PCSALTO ----- OBJETIVO
 # configuramos en local en etc/proxychains.conf socks4  127.0.0.1 9050
+# SALTO = IP maquina de salto
+# OBJETIVO = IP maquina objetivo a traves de salto
 
 ssh -f admin@SALTO -L 9050:localhost:222 -N 
 sudo nmap OBJETIVO -Pn -v --proxy socks4://127.0.0.1:9050 -sTV -F -reason
