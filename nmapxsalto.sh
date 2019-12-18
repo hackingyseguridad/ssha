8e63549edd8ba902fb5590b8d36dae3bb91f4202
hackingyseguridad <antonio_taboada@hotmail.es>
2019-12-18 21:58:51 +0100

    Update nmapxsalto.sh

--- nmapxsalto.sh
+++ nmapxsalto.sh
@@ -7,4 +7,4 @@
 
 ssh -f admin@SALTO -L 9050:localhost:222 -N 
 sudo nmap OBJETIVO -Pn -v --proxy socks4://127.0.0.1:9050 -sTV -F -reason
-sudo proxychains -q nmap -Pn OBJETIVO -sTV -reason
+sudo proxychains nmap OBJETIVO -q -Pn -sTV -reason
