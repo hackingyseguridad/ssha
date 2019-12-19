71fd86b298bbf9b22e1d7fb33fe5f02ff13a0161
hackingyseguridad <antonio_taboada@hotmail.es>
2019-12-19 08:04:32 +0100

    Update nmapxsalto.sh

--- nmapxsalto.sh
+++ nmapxsalto.sh
@@ -5,6 +5,6 @@
 # SALTO = IP maquina de salto
 # OBJETIVO = IP maquina objetivo a traves de salto
 
-ssh -f admin@SALTO -L 9050:localhost:222 -N 
+ssh -N -f admin@SALTO -L 127.0.0.1:9050:OBJETIVO:22 -N
 sudo nmap OBJETIVO -Pn -v --proxy socks4://127.0.0.1:9050 -sTV -F -reason
 sudo proxychains nmap OBJETIVO -q -Pn -sTV -reason
