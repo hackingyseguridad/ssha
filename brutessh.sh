1613aad0dea6833ea4c0dbccc44162f89c9a55fb
hackingyseguridad <antonio_taboada@hotmail.es>
2020-03-14 16:45:55 +0100

    Update brutessh.sh

--- brutessh.sh
+++ brutessh.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 cat << "INFO"
 
-bruteSSH
+                                                          ..
+                               ,,,                         MM .M
+                           ,!MMMMMMM!,                     MM MM  ,.
+   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
+ . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
+;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
+ M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
+ 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
+  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
+ MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
+ 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
+  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
+   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
+  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
+  MMM      :M  'MMMMMMMMMMMMM.MMMMM.MMMMMMMMMM'.MM  MM:M.    'MMMMM
+ .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'
+AMMM..MMMMM:M.    :M.'MMMMMMMMMMMMMMMMMMMM'.MM'     MM''''''''''''
+MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
+ '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
+            MMM.            'MMMM!MMMM'            .MM
+             MMM.             '''   ''            .MM'
+              MMM.            bruteSSH           MMM'
+               MMMM            ,.J.JJJJ.       .MMM'
+                MMMM.       'JJJJJJJ'JJJM   CMMMMM
+                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
+                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
+                      'MMMMMMMMM'JJJJJ JJJJJ'
+                         ''MMMMMMJJJJJJJJJJ'
+                                 'JJJJJJJJ'
 
 INFO
 if [ -z "$1" ]; then
@@ -14,4 +42,4 @@ if [ -z "$1" ]; then
 fi
 echo
 echo
-medusa -h $1 -U usuarios.txt -P claves.txt -M ssh
+medusa -f -h $1 -U usuarios.txt -P claves.txt -M ssh
