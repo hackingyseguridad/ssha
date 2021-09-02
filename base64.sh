9fea90c5e5ef06a2f1eb2b802cdf86346996089f
hackingyseguridad <antonio_taboada@hotmail.es>
2021-09-02 10:24:25 +0200

    Update base64.sh

--- base64.sh
+++ base64.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
 
+# Genera para fuerza bruta listado con credenciales en base64
+
+USERNAME_WORDLIST="usuarios.txt"
+PASSWORD_WORDLIST="claves.txt"
+
+while IFS= read -r user
+do
+    while IFS= read -r password
+    do
+        printf "%s:%s" "$user" "$password" |base64
+    done < "$PASSWORD_WORDLIST"
+done < "$USERNAME_WORDLIST"
