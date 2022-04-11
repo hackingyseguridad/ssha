81ed0e3ca74e1be88b52b14a6ed7a652730e586a
hackingyseguridad <antonio_taboada@hotmail.es>
2022-04-11 11:34:32 +0200

    Create actualizar.sh

---
+++ actualizar.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo 
+echo "... actualizando diccionarios ...   2021 hackingyseguridad.com "
+wget https://raw.githubusercontent.com/hackingyseguridad/fuzzer/master/diccionario.txt -q -O diccionario.txt
