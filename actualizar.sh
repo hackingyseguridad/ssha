48e0f240f9f82ffbcc43c5afacce009c1c5060ae
hackingyseguridad <antonio_taboada@hotmail.es>
2022-04-27 13:37:20 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 
-echo "... actualizando diccionarios ...   2021 hackingyseguridad.com "
+echo "... actualizando diccionarios ...   2022 hackingyseguridad.com "
 wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -q -O diccionario.txt
 wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O usuarios.txt
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O claves.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -q -O claves.txt
