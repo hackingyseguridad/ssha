d358158acae4de2f094a9ec1353c25ec4e6ad69c
hackingyseguridad <antonio_taboada@hotmail.es>
2022-06-13 10:03:48 +0200

    Update changedns.sh

--- changedns.sh
+++ changedns.sh
@@ -1,2 +1,5 @@
+# ejecutar comando en remoto!
+# ssh admin@localhost -p 22 "/usr/bin/uname"
+# ssh admin@localhost -p 22 "sudo -S /usr/bin/uname"
 
 ssh usuario@IP -echo "nameserver 127.0.0.1" > /etc/resolv.conf
