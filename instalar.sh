921a02148b7d9abcba557d419d35a34dd4c91e82
hackingyseguridad <antonio_taboada@hotmail.es>
2023-02-01 09:35:41 +0100

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,4 +1,15 @@
+
 #!/bin/sh
+# hacking y seguridad .COM 2023
+echo "Instalando .."
+echo
 apt-get install ncrack
 apt-get install medusa
 apt-get install nmap
+cp ssha /sbin
+chmod 777 /sbin/ssha
+cp sshb /sbin
+chmod 777 /sbin/sshb
+cp sshc /sbin
+chmod 777 /sbin/sshc
+
