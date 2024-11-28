c908a63f8c5d80d02ca3cef6edb5033cdbf5878c
hackingyseguridad <antonio_taboada@hotmail.es>
2024-11-28 17:20:20 +0100

    Create sshaudit.sh

---
+++ sshaudit.sh
@@ -0,0 +1,9 @@
+# 
+#
+# audit ssh (R) hackingyseguridad,.   2024
+
+nmap -Pn -sT -p22 $1 -sC # Send default nmap scripts for SSH
+nmap -Pn -sT -p22 $1 -sV # Retrieve version
+nmap -Pn -sT -p22 $1 --script ssh2-enum-algos # Retrieve supported algorythms
+nmap -Pn -sT -p22 $1 --script ssh-hostkey --script-args ssh_hostkey=full # Retrieve weak keys
+nmap -Pn -sT -p22 $1 --script ssh-auth-methods  --script-args="ssh.user=root"  # Check authentication methods
