a4cc08d185be7b0ef7e8e1aecb6d42e75d766d3b
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-24 22:15:20 +0100

    Update sshscan2.sh

--- sshscan2.sh
+++ sshscan2.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 # Prueba servicio ssh y extrea la version
 # Lee IPs de ip.txt y ejecuta nxc ssh
 # hackingyseguridad.com 2026
 
-for n in `cat ip.txt`; do echo; timeout 22 nxc ssh $n ; done
+for n in `cat ip.txt`; do echo; timeout 30 nxc ssh $n ; done
