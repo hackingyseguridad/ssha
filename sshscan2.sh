29fb45aab0880123a8dc19b3c1b20f6a87062113
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-24 22:07:49 +0100

    Update sshscan2.sh

--- sshscan2.sh
+++ sshscan2.sh
@@ -4,8 +4,4 @@
 # Lee IPs de ip.txt y ejecuta nxc ssh
 # hackingyseguridad.com 2026
 
-while IFS= read -r IP; do
-    if [ -n "$IP" ]; then
-        nxc ssh "$IP"
-    fi
-done < ip.txt
+for n in `cat ip.txt`; do echo; timeout 22 nxc ssh $n ; done
