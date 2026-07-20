3eaa6012ee7ec160e4508510ececb81dd617e7d6
hackingyseguridad <antonio_taboada@hotmail.es>
2026-07-20 12:01:46 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -8,22 +8,22 @@ echo
 echo "... actualizando diccionarios ...  "
 chmod 777 *
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -q -O diccionario.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -q -O diccionario.txt -4
 echo "diccionario.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O usuarios.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O usuarios.txt -4 
 echo "usuarios.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios0.txt -q -O usuarios0.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios0.txt -q -O usuarios0.txt -4
 echo "usuarios0.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -q -O claves.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -q -O claves.txt -4
 echo "claves.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -q -O claves2.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -q -O claves2.txt -4
 echo "claves2.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves0.txt -q -O claves0.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves0.txt -q -O claves0.txt -4
 echo "claves0.txt"
 echo ".."
 echo "..."
